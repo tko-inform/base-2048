fce33a3341c66ee7e30a26e01160bed990735e72
georgy <httpcatharsis@gmail.com>
2019-08-22 20:19:09 +0300

    Mentioned the authors of Base58, base-x in pom.xml.

--- src/main/java/safezero/base2048/BaseX.java
+++ src/main/java/safezero/base2048/BaseX.java
@@ -10,8 +10,6 @@
  * Ported to JavaScript by Stefan Thomas
  * Merged Buffer refactorings from base58-native by Stephen Pair
  * Copyright (c) 2013 BitPay Inc
- *
- * And then ported back to Java.
  */
 
 package safezero.base2048;
